2a609804ddb462be71da43af84c9056f7ca94724
Chiran Agalawatte <cba_44@yahoo.com>
2022-03-04 21:32:53 +0530

    first commit

---
+++ src/main/java/com/cba44/springboot/basics/SpringBoot28minutesApplication.java
@@ -0,0 +1,13 @@
+package com.cba44.springboot.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot28minutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot28minutesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cba44/springboot/basics/SpringBoot28minutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cba44.springboot.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot28minutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
